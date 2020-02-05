b44ce2c1e4ce6546c96e0a6542d8e367d189ccbf
Twilio <twilio-ci@twilio.com>
2020-02-05 19:16:49 +0000

    Release 2.5.1

--- rest.go
+++ rest.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Version represents the current version of the rest library
-const Version = "2.5.0"
+const Version = "2.5.1"
 
 // Method contains the supported HTTP verbs.
 type Method string
